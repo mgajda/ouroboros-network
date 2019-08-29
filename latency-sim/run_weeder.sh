c67719a62c909a8f8af850354a009e70a241ebaa
Michal J. Gajda <mjgajda@migamake.com>
2019-08-29 09:13:15 +0200

    WIP on removing weeds.

--- latency-sim/run_weeder.sh
+++ latency-sim/run_weeder.sh
@@ -1,4 +1,24 @@
 #!/bin/env bash
 
+#shopt -s extglob
 
-weeder --build --test
+mkdir -p src-weeder sim-weeder test-weeder;
+
+unliterate() {
+  echo "From $1 to $2";
+  for input in $1/*.lhs; do
+    markdown-unlit -h `basename $input .lhs` $input $2/`basename $input .lhs`.hs;
+  done
+  if [[ "$1/*" =~ "*.hs" ]]; then
+    for input in $1/*.hs; do
+      cp $input $2/`basename $input`
+    done
+  fi;
+}
+unliterate src  src-weeder
+unliterate test test-weeder
+unliterate sim  sim-weeder
+
+stack build
+
+#weeder --build --test
